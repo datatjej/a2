5808c5e4eabe2e60a5af9dfb98d5692505a00933
Tova Erbén <guserbto@GU.GU.SE@mltgpu.flov.gu.se>
2024-11-02 10:39:08 +0100

    Add more layers to the model - runs but does not improve accuracy

--- wikiart.py
+++ wikiart.py
@@ -60,27 +60,32 @@ class WikiArtDataset(Dataset):
 
 class WikiArtModel(nn.Module):
     def __init__(self, num_classes=27):
-        super().__init__()
+        super(WikiArtModel, self).__init__()
+        self.conv2d_1 = nn.Conv2d(3, 32, kernel_size=4, padding=2)  # Output: (32, 416, 416)
+        self.maxpool2d_1 = nn.MaxPool2d(kernel_size=2, padding=1)   # Output: (32, 209, 209)
+        
+        self.conv2d_2 = nn.Conv2d(32, 64, kernel_size=4, padding=2) # Output: (64, 209, 209)
+        self.maxpool2d_2 = nn.MaxPool2d(kernel_size=2, padding=1)   # Output: (64, 106, 106) (rounded up from 105.5)
 
-        self.conv2d = nn.Conv2d(3, 1, (4,4), padding=2)
-        self.maxpool2d = nn.MaxPool2d((2,2), padding=0)
         self.flatten = nn.Flatten()
-        self.batchnorm1d = nn.BatchNorm1d(208*208)
-        self.linear1 = nn.Linear(208*208, 300)
+        self.batchnorm1d = nn.BatchNorm1d(64 * 106 * 106)
+        self.linear1 = nn.Linear(64 * 106 * 106, 300)
         self.dropout = nn.Dropout(0.3)
         self.relu = nn.ReLU()
         self.linear2 = nn.Linear(300, num_classes)
         self.softmax = nn.LogSoftmax(dim=1)
 
-
     def forward(self, image):
-        output = self.conv2d(image)
-        #print("convout {}".format(output.size()))
-        output = self.maxpool2d(output)
-        #print("poolout {}".format(output.size()))        
+        output = self.conv2d_1(image)
+        output = self.relu(output)
+        output = self.maxpool2d_1(output)
+
+        output = self.conv2d_2(output)
+        output = self.relu(output)
+        output = self.maxpool2d_2(output)
+
         output = self.flatten(output)
         output = self.batchnorm1d(output)
-        #print("poolout {}".format(output.size()))        
         output = self.linear1(output)
         output = self.dropout(output)
         output = self.relu(output)
